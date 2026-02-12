5bb7634d4ceb8d91a1cd0d345db57ff9752dee23
tsutowski <dizzytyler45@gmail.com>
2026-02-12 16:43:59 -0500

    adding justtcg api call

--- randomcard.js
+++ randomcard.js
@@ -1,9 +1,11 @@
 const url1 = "https://api.scryfall.com/cards/random";
+const url2 = 'https://api.justtcg.com/v1/cards'
+const TCGAPI_KEY = 'tcg_a2f7d4971ff2448b87a5dd24d8aa064e'
+fetch_image_data();
 
 
-fetchdata();
 
-async function fetchdata() {
+async function fetch_image_data() {
     try {
         const response = await fetch(url1);
         if (!response.ok) {
@@ -12,6 +14,7 @@ async function fetchdata() {
         }
         const data = await response.json();
         const CardPic = data.image_uris.normal;
+        const sid = data.id;
         const img = document.getElementById("cardImage");
 
         img.src = CardPic;
@@ -20,5 +23,23 @@ async function fetchdata() {
     } catch (error) {
         console.error(error);
     }
+
+}
+
+async function fetch_price_data() {
+    try {
+        const response = await fetch(url2, { headers: { 'X-API-KEY': TCGAPI_KEY } });
+        url2.searchParams.append('scryfallId', sid);
+        if (!response.ok) {
+            throw new Error("No data found");
+        }
+        const result = await response.json;
+        const card = result.data;
+    } catch (error) {
+        console.error("error fetching price data");
+    }
+
+
+
 
 }
