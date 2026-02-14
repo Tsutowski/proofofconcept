c0d32f4df0c5e520b3c919fa7a9cdc15aec538e2
tsutowski <dizzytyler45@gmail.com>
2026-02-14 02:18:38 -0500

    t

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # proofofconcept
 LINK TO SITE HERE:
 https://tsutowski.github.io/proofofconcept/
+
+proof of concept repo for CIS3296
--- randomcard.js
+++ randomcard.js
@@ -1,33 +1,34 @@
 const url1 = "https://api.scryfall.com/cards/random";
-const USER_AGENT = 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:147.0) Gecko/20100101 Firefox/147.0';
+
+const USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:147.0) Gecko/20100101 Firefox/147.0';
 
 fetch_image_data();
 
 
 async function fetch_image_data() {
     try {
-        const response = await fetch(url1, { headers: { 'User-Agent': USER_AGENT, 'Accept': '*\*' } });
+        const response = await fetch(url1, { mode: 'cors', headers: { 'User-Agent': USER_AGENT, 'Accept': '*\*', 'Access-Control-Allow-Origin': 'https://tsutowski.github.io/proofofconcept/' } });
         if (!response.ok) {
             throw new Error("no data found");
 
         }
-        //console.log(response.json());
+        console.log(response.json());
 
-        const data = await response.json();
-        const CardPic = data.image_uris.normal;
-        if (data.foil == true) {
-            cprice = data.prices.usd_foil;
-        } else {
-            cprice = data.prices.usd;
-        }
-        const price = cprice;
-        const box = document.getElementById("pricefield");
-        box.innerHTML = box.innerHTML.replace("CardPrice", price);
+        // const data = await response.json();
+        // const CardPic = data.image_uris.normal;
+        // if (data.foil == true) {
+        //     cprice = data.prices.usd_foil;
+        // } else {
+        //     cprice = data.prices.usd;
+        // }
+        // const price = cprice;
+        // const box = document.getElementById("pricefield");
+        // box.innerHTML = box.innerHTML.replace("CardPrice", price);
 
-        const img = document.getElementById("cardImage");
+        // const img = document.getElementById("cardImage");
 
-        img.src = CardPic;
-        img.style.display = "block";
+        // img.src = CardPic;
+        // img.style.display = "block";
 
     } catch (error) {
         console.error(error);
